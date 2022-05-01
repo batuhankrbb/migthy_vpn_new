43992fd392a5943c06121bed43803a82f850660f
Batuhan <batuhankarababa@gmail.com>
2022-05-01 19:04:30 +0300

    add open ad

--- lib/component/AdmobComponent.dart
+++ lib/component/AdmobComponent.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/foundation.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:mightyvpn/main.dart';
 import '../utils/AdMobUtils.dart';
+import 'dart:io' show Platform;
+
+import '../utils/constant.dart';
 
 RewardedAd? rewardedAd;
 InterstitialAd? myInterstitial;
-
 
 Future<void> loadInterstitialAd() async {
   InterstitialAd.load(
@@ -82,5 +85,124 @@ Future<void> showRewardedAd({required VoidCallback onWinReward}) async {
   rewardedAd = null;
 }
 
+AppOpenAd? openAd;
 
+Future<void> loadOpenAd() async {
+  if (globalStore.openAdShowed) {
+    return;
+  }
+  globalStore.openAdShowed = true;
+  await AppOpenAd.load(
+      adUnitId: AD_MOB_OPEN_AD_ID,
+      request: AdRequest(),
+      adLoadCallback: AppOpenAdLoadCallback(onAdLoaded: (ad) {
+        openAd = ad;
+        openAd!.fullScreenContentCallback = FullScreenContentCallback(
+          onAdFailedToShowFullScreenContent: (ad, error) {
+            ad.dispose();
+            openAd = null;
+          },
+          onAdDismissedFullScreenContent: (ad) {
+            ad.dispose();
+            openAd = null;
+          },
+        );
+        openAd!.show();
+      }, onAdFailedToLoad: (LoadAdError error) {
+        openAd = null;
+      }),
+      orientation: AppOpenAd.orientationPortrait);
+}
 
+class AppOpenAdManager {
+  /// Maximum duration allowed between loading and showing the ad.
+  final Duration maxCacheDuration = Duration(hours: 4);
+
+  /// Keep track of load time so we don't show an expired ad.
+  DateTime? _appOpenLoadTime;
+
+  AppOpenAd? _appOpenAd;
+  bool _isShowingAd = false;
+
+  /// Whether an ad is available to be shown.
+  bool get isAdAvailable {
+    return _appOpenAd != null;
+  }
+
+  void loadAd() {
+    AppOpenAd.load(
+      adUnitId: getOpenAppAdUnitId(),
+      orientation: AppOpenAd.orientationPortrait,
+      request: AdRequest(),
+      adLoadCallback: AppOpenAdLoadCallback(
+        onAdLoaded: (ad) {
+          _appOpenAd = ad;
+          _appOpenLoadTime = DateTime.now();
+        },
+        onAdFailedToLoad: (error) {
+          print('AppOpenAd failed to load: $error');
+          // Handle the error.
+        },
+      ),
+    );
+  }
+
+  void showAdIfAvailable() {
+    if (!isAdAvailable) {
+      print('Tried to show ad before available.');
+      loadAd();
+      return;
+    }
+    if (_isShowingAd) {
+      print('Tried to show ad while already showing an ad.');
+      return;
+    }
+    if (DateTime.now().subtract(maxCacheDuration).isAfter(_appOpenLoadTime!)) {
+      print('Maximum cache duration exceeded. Loading another ad.');
+      _appOpenAd!.dispose();
+      _appOpenAd = null;
+      loadAd();
+      return;
+    }
+    // Set the fullScreenContentCallback and show the ad.
+    _appOpenAd!.fullScreenContentCallback = FullScreenContentCallback(
+      onAdShowedFullScreenContent: (ad) {
+        _isShowingAd = true;
+        print('$ad onAdShowedFullScreenContent');
+      },
+      onAdFailedToShowFullScreenContent: (ad, error) {
+        print('$ad onAdFailedToShowFullScreenContent: $error');
+        _isShowingAd = false;
+        ad.dispose();
+        _appOpenAd = null;
+      },
+      onAdDismissedFullScreenContent: (ad) {
+        print('$ad onAdDismissedFullScreenContent');
+        _isShowingAd = false;
+        ad.dispose();
+        _appOpenAd = null;
+        loadAd();
+      },
+    );
+  }
+}
+
+class AppLifecycleReactor {
+  final AppOpenAdManager appOpenAdManager;
+
+  AppLifecycleReactor({required this.appOpenAdManager});
+
+  void listenToAppStateChanges() {
+    AppStateEventNotifier.startListening();
+    AppStateEventNotifier.appStateStream
+        .forEach((state) => _onAppStateChanged(state));
+  }
+
+  void _onAppStateChanged(AppState appState) {
+    // Try to show an app open ad if the app is being resumed and
+    // we're not already showing an app open ad.
+    if (appState == AppState.foreground) {
+      appOpenAdManager.showAdIfAvailable();
+    }
+  }
+}
