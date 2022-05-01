4a251afaae31e1778d551b0f38d82405b3457e62
nika <nikakalandias@gmail.com>
2022-05-01 15:50:44 +0300

    arabic localize

--- lib/language/LanguageAr.dart
+++ lib/language/LanguageAr.dart
@@ -178,53 +178,59 @@ class LanguageAr extends BaseLanguage {
       "فشل اختبار التحميل! الرجاء التحقق من اتصال الانترنت الخاص بك";
 
   @override
-  String get onboard1Description => "throw UnimplementedError();";
+  String get onboard1Description =>
+      "أكثر من 1000 خادم عالي الجودة في حوالي 80 دولة. كل منهم قدمه لنا. ٪ 99.99 الجهوزية. احصل على أفضل خادم VPN حول العالم بنقرة واحدة.";
 
   @override
-  String get onboard1Title => "throw UnimplementedError();";
+  String get onboard1Title => "أكثر من 120 موقعا";
 
   @override
-  String get onboard2Description => "throw UnimplementedError();";
+  String get onboard2Description =>
+      "احصل على وصول إلى جميع المواقع المحجوبة واستمتع بنطاق ترددي غير محدود. الوصول إلى مواقعك وتطبيقاتك المفضلة في جميع أنحاء العالم بسرعات عالية";
 
   @override
-  String get onboard2Title => "throw UnimplementedError();";
+  String get onboard2Title => "قم بإلغاء حظر كل شيء";
 
   @override
-  String get onboard3Description => " throw UnimplementedError()";
+  String get onboard3Description =>
+      "هل سئمت من التطبيقات البطيئة الأخرى الموجودة هناك؟ جرب خادمنا السريع x8 المحسن. لن تواجهك مشاكل بشأن سرعة الإنترنت مرة أخرى.";
 
   @override
-  String get onboard3Title => "throw UnimplementedError()";
+  String get onboard3Title => "8x اتصال إنترنت أسرع";
 
   @override
-  String get bestServerText => throw UnimplementedError();
+  String get bestServerText =>
+      "هذا هو أفضل خادم لدينا. إنه متاح فقط للمستخدمين المميزين.";
 
   @override
-  String get buyAndContinueText => throw UnimplementedError();
+  String get buyAndContinueText => "الشراء والاستمرار";
 
   @override
-  String get getPremiumText => throw UnimplementedError();
+  String get getPremiumText => "إحصل على الأفضل";
 
   @override
-  String get getPremiumUnlockLongText => throw UnimplementedError();
+  String get getPremiumUnlockLongText =>
+      "احصل على قسط لفتح جميع الخوادم إلى الأبد وإزالة الإعلانات ، أو مشاهدة الإعلانات لفتحها مرة واحدة.";
 
   @override
-  String get moreThan3ErrorText => throw UnimplementedError();
+  String get moreThan3ErrorText =>
+      "لا يمكنك فتح أكثر من 3 خوادم من خلال مشاهدة الإعلانات. احصل على قسط للوصول إلى جميع الخوادم إلى الأبد.";
 
   @override
-  String get perYearText => throw UnimplementedError();
+  String get perYearText => "كل سنة";
 
   @override
-  String get selectServerText => throw UnimplementedError();
+  String get selectServerText => "حدد الخادم";
 
   @override
-  String get themePremiumText => throw UnimplementedError();
+  String get themePremiumText => "الموضوع متاح فقط للمستخدمين المميزين.";
 
   @override
-  String get thisIsPremiumServerText => throw UnimplementedError();
+  String get thisIsPremiumServerText => "هذا خادم ممتاز";
 
   @override
-  String get unlockForeverText => throw UnimplementedError();
+  String get unlockForeverText => "إفتح للأبد";
 
   @override
-  String get watchAdUnlockOnceText => throw UnimplementedError();
+  String get watchAdUnlockOnceText => "مشاهدة الإعلان لفتح مرة واحدة.";
 }
