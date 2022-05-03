cad29ecafc4d80c610af1ced552d51d7bf5f2ebd
Batuhan <batuhankarababa@gmail.com>
2022-05-03 16:22:45 +0300

    update constants

--- lib/utils/constant.dart
+++ lib/utils/constant.dart
@@ -14,6 +14,7 @@ import 'package:nb_utils/nb_utils.dart';
 
 
 //* -------- KEYS --------
+
 const revenueCatKey = 'goog_HkYGxKQJURUnLdBZHGuFKuHUXKz';
 const mixpanelKey = '1d81f3fb8497319e960929e845d790e2';
 
